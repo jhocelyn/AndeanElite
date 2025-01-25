26c0df5d96cf1a75a8d8d2d3e02c480633bcffb0
Alecit13 <u20221c488@upc.edu.pe>
2025-01-25 10:19:01 -0500

    update

--- src/app/pages/Destionations-pages/destinations/destinations.component.ts
+++ src/app/pages/Destionations-pages/destinations/destinations.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import {NavbarComponent} from '../../../shared/components/navbar/navbar.component';
 import {FooterComponent} from '../../../shared/components/footer/footer.component';
 import {RouterLink} from '@angular/router';
+import {TranslatePipe} from '@ngx-translate/core';
 
 @Component({
   selector: 'app-destinations',
   standalone: true,
-  imports: [NavbarComponent, FooterComponent, RouterLink],
+  imports: [NavbarComponent, FooterComponent, RouterLink, TranslatePipe],
   templateUrl: './destinations.component.html',
   styleUrl: './destinations.component.css'
 })
