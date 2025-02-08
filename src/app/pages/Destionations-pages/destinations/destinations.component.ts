11804ce3e1ac59191acb744667e021a9b30f8eaf
Alecit13 <u20221c488@upc.edu.pe>
2025-02-07 19:25:24 -0500

    refactor: jpg to webp

--- src/app/pages/Destionations-pages/destinations/destinations.component.ts
+++ src/app/pages/Destionations-pages/destinations/destinations.component.ts
@@ -13,5 +13,13 @@ import {LazyLoadImageModule} from 'ng-lazyload-image';
   styleUrl: './destinations.component.css'
 })
 export class DestinationsComponent {
+  destinations = [
+    { name: 'Arequipa', img: 'assets/img/destinations/arequipa/arequipa-church.webp', route: '/destination/arequipa/packages' },
+    { name: 'Cusco', img: 'assets/img/destinations/cusco/traveling-in-peru.webp', route: '/destination/cusco/packages' },
+    { name: 'Puerto Maldonado', img: 'assets/img/home/amazon-jungle.webp', route: '/destination/puerto-maldonado/packages' },
+    { name: 'Lima', img: 'assets/img/destinations/lima/scenery-by-the-ocean.webp', route: '/destination/lima/packages' },
+    { name: 'Ica', img: 'assets/img/destinations/ica/panoramic-view-of-huacachina.webp', route: '/destination/ica/packages' },
+    { name: 'Nazca', img: 'assets/img/destinations/nazca/beautiful-shot-of-nazca.webp', route: '/destination/nazca/packages' }
+  ];
 
 }
