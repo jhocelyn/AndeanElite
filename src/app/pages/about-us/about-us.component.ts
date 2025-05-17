5a36ed2ef55298d713cb1b0323570ee9375d5de0
Alecit13 <u20221c488@upc.edu.pe>
2025-05-17 12:26:42 -0500

    feat

--- src/app/pages/about-us/about-us.component.ts
+++ src/app/pages/about-us/about-us.component.ts
@@ -2,11 +2,7 @@ import { Component } from '@angular/core';
 import {TranslatePipe, TranslateService} from '@ngx-translate/core';
 import {BannerComponent} from "../../shared/components/general/banner/banner.component";
 import {NgForOf} from '@angular/common';
-interface TeamMember {
-  name: string;
-  description: string;
-  image: string;
-}
+
 @Component({
   selector: 'app-about-us',
   standalone: true,
@@ -15,21 +11,13 @@ interface TeamMember {
   styleUrl: './about-us.component.css'
 })
 export class AboutUsComponent {
-  team: TeamMember[] = [];
 
   constructor(private translate: TranslateService) {}
 
   ngOnInit(): void {
-    this.loadTeam();
     this.translate.onLangChange.subscribe(() => {
-      this.loadTeam();
     });
   }
 
-  loadTeam(): void {
-    this.translate.get('ABOUT-US.OUR_TEAM.TEAM').subscribe((data: TeamMember[]) => {
-      this.team = data;
-    });
-  }
 
 }
