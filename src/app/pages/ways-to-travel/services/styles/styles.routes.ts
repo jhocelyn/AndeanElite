ca6903f1bc5ed04a153f6ef54d17d9c7eb69bb88
Alecit13 <u20221c488@upc.edu.pe>
2025-05-29 10:05:50 -0500

    feat

--- src/app/pages/ways-to-travel/services/styles/styles.routes.ts
+++ src/app/pages/ways-to-travel/services/styles/styles.routes.ts
@@ -7,12 +7,9 @@ export const stylesRoutes:Routes=[
     path:'',component:LayoutComponent,
     children:[
       {path:'',component:ListStylesComponent},
-      {path:'premium', loadChildren: () => import('./styles/premium/premium.routes').then(m => m.premiumRoutes)},
       {path:'deluxe', loadChildren: () => import('./styles/deluxe/deluxe.routes').then(m => m.deluxeRoutes)},
       {path:'comfort', loadChildren: () => import('./styles/comfort/comfort.routes').then(m => m.comfortRoutes)},
-      {path:'price-sale', loadChildren: () => import('./styles/price-sale/price-sale.routes').then(m => m.priceSaleRoutes)},
-      {path:'super-deals', loadChildren: () => import('./styles/super-deals/super-deals.routes').then(m => m.superDealsRoutes)},
-
+      {path:'super-deals', loadChildren: () => import('./styles/super-deals/super-deals.routes').then(m => m.superDealsRoutes)}
     ]
   }
 ]
