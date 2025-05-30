dc835ae11b0fba90e0dadb1ea1b472fb8e42bf1b
Alecit13 <u20221c488@upc.edu.pe>
2025-05-30 14:02:06 -0500

    feat

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
   import { Component } from '@angular/core';
   import {NavigationEnd, Router, RouterOutlet} from '@angular/router';
   import {NavbarComponent} from './shared/components/Important/navbar/navbar.component';
-  import {FooterComponent} from './shared/components/Important/footer/footer.component';
-  import {filter} from 'rxjs';
+  import {FooterComponent} from './shared/components/Important/footer/footer.component';import { filter } from 'rxjs';
 
-  declare let fbq: Function;  // <-- Esto es clave
+  declare global {
+    interface Window { fbq: (...args: any[]) => void; }
+  }
 
   @Component({
     selector: 'app-root',
@@ -15,12 +16,16 @@
   })
   export class AppComponent {
     title = 'Andeanelite';
-    constructor(private router:Router) {
+    constructor(private router: Router) {
       this.router.events.pipe(
         filter(event => event instanceof NavigationEnd)
-      ).subscribe(()=>{
-        fbq('track', 'PageView');
-      })
+      ).subscribe(() => {
+        // Verificación para evitar error si fbq aún no está cargado
+        if (typeof window !== 'undefined' && typeof window.fbq === 'function') {
+          window.fbq('track', 'PageView');
+        } else {
+          console.warn('Meta Pixel (fbq) aún no está cargado.');
+        }
+      });
     }
-
   }
