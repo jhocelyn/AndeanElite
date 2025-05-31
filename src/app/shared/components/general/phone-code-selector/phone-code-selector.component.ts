e3ec0c0eec2d0d43929b033e441db9564849c675
Alecit13 <u20221c488@upc.edu.pe>
2025-05-31 11:57:22 -0500

    feat

--- src/app/shared/components/general/phone-code-selector/phone-code-selector.component.ts
+++ src/app/shared/components/general/phone-code-selector/phone-code-selector.component.ts
@@ -1,31 +1,38 @@
-import {Component, EventEmitter, Output} from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import {PhoneCodeService} from '../../../../services/phone-code.service';
 import {FormsModule} from '@angular/forms';
-import {NgForOf} from '@angular/common';
+import {NgForOf, NgIf} from '@angular/common';
+import {TranslatePipe} from '@ngx-translate/core';
 
 @Component({
   selector: 'app-phone-code-selector',
   standalone: true,
   imports: [
     FormsModule,
-    NgForOf
+    NgForOf,
+    TranslatePipe,
+    NgIf
   ],
   templateUrl: './phone-code-selector.component.html',
   styleUrl: './phone-code-selector.component.css'
 })
-export class PhoneCodeSelectorComponent {
+export class PhoneCodeSelectorComponent implements OnInit{
   phoneCodes: any[] = [];
-  selectedPhoneCode: string = '';
 
   // Define el evento que emitirá el valor seleccionado
   @Output() phoneCodeSelected = new EventEmitter<string>();
 
   constructor(private phoneCodeService: PhoneCodeService) {}
 
+  searchTerm: string = '';
+  filteredPhoneCodes: any[] = [];
+  showDropdown: boolean = false;
+
   ngOnInit(): void {
     this.phoneCodeService.getPhoneCodes().subscribe(
       (data) => {
         this.phoneCodes = data;
+        this.filteredPhoneCodes = data;
       },
       (error) => {
         console.error('Error al cargar los códigos telefónicos:', error);
@@ -33,9 +40,25 @@ export class PhoneCodeSelectorComponent {
     );
   }
 
-  onPhoneCodeChange(): void {
-    // Emite el código telefónico seleccionado al componente padre
-    this.phoneCodeSelected.emit(this.selectedPhoneCode);
+  filterCountries(): void {
+    const term = this.searchTerm.toLowerCase();
+    this.filteredPhoneCodes = this.phoneCodes.filter(country =>
+      country.name.toLowerCase().includes(term) ||
+      country.phoneCode.includes(term)
+    );
+  }
+
+  selectCountry(country: any): void {
+    this.searchTerm = `${country.flagEmoji} ${country.name} (${country.phoneCode})`;
+    this.phoneCodeSelected.emit(country.phoneCode);
+    this.showDropdown = false;
+  }
+
+  hideDropdown(): void {
+    // Esperar un momento para permitir que (mousedown) se ejecute
+    setTimeout(() => {
+      this.showDropdown = false;
+    }, 200);
   }
 
 }
