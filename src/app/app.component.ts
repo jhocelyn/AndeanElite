32bf4fff21caf73d22ffe220b0975b8491b89b50
Alecit13 <u20221c488@upc.edu.pe>
2025-06-16 17:17:35 -0500

    fix:

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,11 @@
-  import { Component } from '@angular/core';
+  import {Component, OnInit} from '@angular/core';
+  import { isPlatformBrowser } from '@angular/common';
+  import { Inject, PLATFORM_ID } from '@angular/core';
   import {NavigationEnd, Router, RouterOutlet} from '@angular/router';
   import {NavbarComponent} from './shared/components/Important/navbar/navbar.component';
-  import {FooterComponent} from './shared/components/Important/footer/footer.component';import { filter } from 'rxjs';
+  import {FooterComponent} from './shared/components/Important/footer/footer.component';
+  import { filter } from 'rxjs';
+  import {NgIf} from '@angular/common';
 
   declare global {
     interface Window { fbq: (...args: any[]) => void; }
@@ -10,13 +14,13 @@
   @Component({
     selector: 'app-root',
     standalone: true,
-    imports: [RouterOutlet, NavbarComponent, FooterComponent],
+    imports: [RouterOutlet, NavbarComponent, FooterComponent, NgIf],
     templateUrl: './app.component.html',
     styleUrl: './app.component.css'
   })
-  export class AppComponent {
+  export class AppComponent implements OnInit{
     title = 'Andeanelite';
-    constructor(private router: Router) {
+    constructor(private router: Router,@Inject(PLATFORM_ID) private platformId: Object) {
       this.router.events.pipe(
         filter(event => event instanceof NavigationEnd)
       ).subscribe(() => {
@@ -28,4 +32,40 @@
         }
       });
     }
+
+    cookiesAccepted = false;
+
+    ngOnInit() {
+      if (isPlatformBrowser(this.platformId)) {
+        this.cookiesAccepted = localStorage.getItem('cookiesAccepted') === 'true';
+        if (this.cookiesAccepted) {
+          this.loadAnalytics();
+        }
+      }
+    }
+
+    acceptCookies() {
+      if (isPlatformBrowser(this.platformId)) {
+        localStorage.setItem('cookiesAccepted', 'true');
+        this.cookiesAccepted = true;
+        this.loadAnalytics();
+      }
+    }
+
+    loadAnalytics() {
+      const script = document.createElement('script');
+      script.src = 'https://www.googletagmanager.com/gtag/js?id=G-LQCL7CW1PK';
+      script.async = true;
+      document.head.appendChild(script);
+
+      const inlineScript = document.createElement('script');
+      inlineScript.innerHTML = `
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'G-LQCL7CW1PK');
+    `;
+      document.head.appendChild(inlineScript);
+    }
+
   }
