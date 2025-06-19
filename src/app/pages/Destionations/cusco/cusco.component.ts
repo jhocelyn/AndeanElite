45378583c5ad4e62da2b5b3aacb610d14451df7a
Alecit13 <u20221c488@upc.edu.pe>
2025-06-19 13:11:41 -0500

    fix: add images of cusco and re-design of destinations-highlights

--- src/app/pages/Destionations/cusco/cusco.component.ts
+++ src/app/pages/Destionations/cusco/cusco.component.ts
@@ -19,24 +19,24 @@ export class CuscoComponent {
   private readonly i18nBase = 'destinations.cusco';
 
   /** banner */
-  imageUrl = 'assets/img/destinations/cusco/Inca-Trail-to-Machu-Picchu.webp';
+  imageUrl = 'assets/img/destinations/cusco/cusco-ciudad-amazing-view.webp';
   overview = `${this.i18nBase}.overview`;
   headline = `${this.i18nBase}.headline`;
 
   /** tarjetas */
   machuPicchuCards: HighlightCard[] = [
-    this.card('classicTrail'),
-    this.card('shortTrail'),
-    this.card('sacredValley'), // reemplaza por los IDs reales restantes
-    this.card('rainbowMountain'),
-    this.card('morayMaras'),
-    this.card('cuscoCity'),
+    this.card('classicTrail','assets/img/destinations/cusco/inca-trail-view.webp'),
+    this.card('shortTrail', 'assets/img/destinations/cusco/inka-trail-amazing-view.webp'),
+    this.card('sacredValley','assets/img/destinations/cusco/sacred-valley.webp'), // reemplaza por los IDs reales restantes
+    this.card('rainbowMountain','assets/img/destinations/cusco/rainbow-mountain-view.webp'),
+    this.card('morayMaras','assets/img/destinations/cusco/moray-view.webp'),
+    this.card('cuscoCity','assets/img/destinations/cusco/cusco-ciudad-noche.webp'),
   ];
 
   /** factoría para no repetir código */
-  private card(id: string): HighlightCard {
+  private card(id: string,imageurl:string): HighlightCard {
     return {
-      image: this.imageUrl,
+      image: imageurl,
       alt:   `${this.i18nBase}.cards.${id}.alt`,
       title: `${this.i18nBase}.cards.${id}.title`,
       body:  `${this.i18nBase}.cards.${id}.body`
